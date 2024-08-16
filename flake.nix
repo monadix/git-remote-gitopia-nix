b3d30571b8204a46015e089623cfc4c4596c92b6
chell4 <arsenijalfeev@gmail.com>
2024-08-16 20:16:36 +0300

    add support for all available systems

--- flake.nix
+++ flake.nix
@@ -5,26 +5,32 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
-
-    git-remote-gitopia = {
-      url = "file+https://server.gitopia.com/releases/gitopia/git-remote-gitopia/v1.8.0/git-remote-gitopia_1.8.0_linux_amd64.tar.gz";
-      flake = false;
-    };
   };
 
-  outputs = { nixpkgs, flake-utils, git-remote-gitopia, ... }: 
+  outputs = { nixpkgs, flake-utils, ... }: 
   flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
   let 
     pkgs = import nixpkgs {
       inherit system;
     };
+    
+    systemArch = if system == "x86_64-linux" then "linux_amd64" 
+      else if system == "i686-linux" then "linux_386"
+      else if system == "x86_64-darwin" then "darwin_amd64"
+      else if system == "aarch64-darwin" then "darwin_arm64"
+      else if system == "aarch32-linux" then "linux_arm"
+      else if system == "aarch64-linux" then "linux_arm64"
+      else "unknown";
 
   in {
     packages.default = pkgs.stdenv.mkDerivation rec {
 
       name = "git-remote-gitopia";
-
-      src = git-remote-gitopia;
+      
+      src = builtins.fetchurl {
+        url = "https://server.gitopia.com/releases/gitopia/git-remote-gitopia/v1.8.0/git-remote-gitopia_1.8.0_${systemArch}.tar.gz";
+        sha256 = "1ampr861mw3yfnaka1ii164h79baic9qzjl9pwy1zc5763nm6lq0";
+      };
       
       unpackPhase = ''
         mkdir -p $out/bin
